1baa3c85155352ea1ca30851e053ef155c67fda9
vahid819 <vahidmomin072@gmail.com>
2025-01-24 15:21:23 +0530

    create navbar componant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '@/components/Navbar'
 
 function Home() {
   return (
     <div>
-      
+      <Navbar />
+      Landing page
     </div>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Button } from './ui/button'
+import Link from 'next/link'
+
+function Navbar() {
+    return (
+        <div className='w-full h-20 bg-transparent flex justify-around items-center'>
+            <div>Logo</div>
+            <div className='flex gap-4'>
+                <Link href='/'
+                    className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-xl">
+                    Home
+                </Link>
+                <Link href='/about'
+                    className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-xl">
+                    About
+                </Link>
+                <Link href='/contact'
+                    className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-xl">
+                    Blogs
+                </Link>
+            </div>
+            <div className='flex gap-4'>
+                <Link href={'/signup'}>
+                    <Button className="transform transition duration-500 hover:scale-110">Sign Up</Button>
+                </Link>
+                <Link href={'/signin'}>
+                    <Button className="transform transition duration-500 hover:scale-110">Sign In</Button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
